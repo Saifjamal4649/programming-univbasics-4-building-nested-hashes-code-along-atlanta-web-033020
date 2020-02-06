4b3db63c8015ed3ab8133a26c1232d9c86a320f7
Saif Jamal <saifjamal4649@gmail.com>
2020-02-06 05:25:26 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,6 +25,5 @@ def bonus
   epic_tragedy[:capulet][:heroine][:status] = ("dead")
   
 
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  puts epic_tragedy
+  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash     epic_tragedy
 end
